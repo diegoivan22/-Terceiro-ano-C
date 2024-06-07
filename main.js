06d80654ebfecdf49d6fd658b220e937a3e50cae
diegoivan22 <114594983+diegoivan22@users.noreply.github.com>
2024-06-07 12:07:37 -0300

    07/06

--- main.js
+++ main.js
@@ -65,11 +65,13 @@ function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
-    if (entropia > 11){
+    if (entropia > 57){
         forcaSenha.classList.add('forte');
-    } else if (entropia > 5 && entropia < 12) {
+    } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (entropia <= 5){
+    } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
     }
+    const valorEtropia = document.querySelector(',entropia');
+    valorEtropia.textContent = "Um computador pode levar até" + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha";
 }
